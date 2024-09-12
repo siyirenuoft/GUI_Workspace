012a196ac4c637ee0eab9986e91fce906ce1585b
siyirenuoft <117426382+siyirenuoft@users.noreply.github.com>
2024-09-12 15:35:18 -0400

    Orgnized

--- Design Verification Files/load_stored_data.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Replace 'your_pickle_file.pkl' with the path to your pickle file
-with open('new2.dsgn', 'rb') as file:
-    data = pickle.load(file)
-
-# Display the contents in a readable form
-print(data)
--- TrashBin/Connor/models/waveform.py
+++
@@ -1,4 +0,0 @@
-class Waveform:
-    def __init__(self, data, sampling_frequency):
-        self.data = data
-        self.sampling_frequency = sampling_frequency
